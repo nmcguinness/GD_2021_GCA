84a8bee7664118afba6640c6d4407bff7886d293
Niall McGuinness <niall.mcguinness@dkit.ie>
2021-10-27 13:04:36 +0100

    Initial commit

---
+++ GD3_2021_GDApp/GD3_2021_GDApp/Main.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace GDApp
+{
+    public class Main : Game
+    {
+        #region Fields
+
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+
+        #endregion Fields
+
+        #region Constructors
+
+        public Main()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        #endregion Constructors
+
+        #region Initialization
+
+        protected override void Initialize()
+        {
+            base.Initialize();
+        }
+
+        #endregion Initialization
+
+        #region Load & Unload Assets
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+        }
+
+        protected override void UnloadContent()
+        {
+            base.UnloadContent();
+        }
+
+        #endregion Load & Unload Assets
+
+        #region Update & Draw
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            base.Draw(gameTime);
+        }
+
+        #endregion Update & Draw
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# GD_2021_GCA
-3DGED group project codebase
+﻿
+**For a markdown cheat sheet see [Markdown Cheat Sheet](https://www.markdownguide.org/cheat-sheet/)**
+
+## 3D Game Engine Development - [GDLibrary & GDApp](https://github.com/nmcguinness/GD3_3_Intro_To_MonoGame.git)
+
+### Further Reading
+
+
+### Tasks - Week 6
+- [ ] Add Time singleton to allow speed/slow time
+- [ ] Explain use of *where* clause in VertexData<T> 
+- [ ] Method overriding with virtual and override
+- [ ] Demo Predicate
+- [ ] Demo generic class and generic method
