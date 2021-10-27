e75c51905b6cdabede1dcaac70c1b48c0e6fcfb0
Niall McGuinness <niall.mcguinness@dkit.ie>
2021-10-27 13:18:54 +0100

    Added Time

--- GD3_2021_GDApp/GD3_2021_GDApp/Main.cs
+++ GD3_2021_GDApp/GD3_2021_GDApp/Main.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Xna.Framework;
+﻿using GDLibrary.Time;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 
@@ -28,7 +29,18 @@ namespace GDApp
 
         protected override void Initialize()
         {
+            InitializeTime();
             base.Initialize();
+        }
+
+        /// <summary>
+        /// Creates Time instance which can be used by game objects that want to slow down, or speed up, time
+        /// </summary>
+        private void InitializeTime()
+        {
+            //add time instance to the components list so that it will be updated
+            var time = Time.GetInstance(this);
+            Components.Add(time);
         }
 
         #endregion Initialization
@@ -54,7 +66,29 @@ namespace GDApp
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
+            DemoTime(gameTime);
+
             base.Update(gameTime);
+        }
+
+        private void DemoTime(GameTime gameTime)
+        {
+            //we can change the scale at any point in the game to modify how time passed
+            if (Keyboard.GetState().IsKeyDown(Keys.OemPlus))
+                Time.Instance.TimeScale = 2;
+            else if (Keyboard.GetState().IsKeyDown(Keys.R))
+                Time.Instance.TimeScale = 1;
+            else if (Keyboard.GetState().IsKeyDown(Keys.OemMinus))
+                Time.Instance.TimeScale = 0.5f;
+
+            System.Diagnostics.Debug.WriteLine("Original:" + gameTime.ElapsedGameTime.Milliseconds + ", Scaled:" + Time.Instance.ElapsedGameTime);
+
+            if (gameTime.ElapsedGameTime.Milliseconds < Time.Instance.ElapsedGameTime)
+                System.Diagnostics.Debug.WriteLine("Time is running fast!");
+            else if (gameTime.ElapsedGameTime.Milliseconds == Time.Instance.ElapsedGameTime)
+                System.Diagnostics.Debug.WriteLine("Time is normal!");
+            else
+                System.Diagnostics.Debug.WriteLine("Time is running slowly!");
         }
 
         protected override void Draw(GameTime gameTime)
---
+++ GD3_2021_GDLibrary/GD3_2021_GDLibrary/Core/Time/Time.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+
+namespace GDLibrary.Time
+{
+    /// <summary>
+    /// Replaces GameTime for any game objects that want to slow down, or speed up, time in the game
+    /// </summary>
+    /// <example>
+    ///     var time = Time.GetInstance(this);  //In Main.cs we create the first and only instance
+    ///     Components.Add(time);               //Add to the component list so that it will be updated
+    /// </example>
+    /// <seealso cref="https://refactoring.guru/design-patterns/singleton/csharp/example"/>
+    public class Time : GameComponent
+    {
+        #region Fields
+
+        private static Time instance;       //singleton instance to allow global accessibility
+        private float elapsedGameTime;            //time between updates
+        private float totalGameTime;            //total elapsed time since start
+        private float timeScale = 1;        //0-1 scale factor used to slow down time (e.g. slo-mo effects)
+
+        #endregion Fields
+
+        #region Properties
+
+        public float TimeScale { get => timeScale; set => timeScale = value >= 0 ? value : 1; } //use to scale time for visual effects
+
+        //note we can only GET the times below, we cannot set them
+        public float UnscaledElapsedGameTime => elapsedGameTime;        //returns unscaled time between updates in ms
+        public float UnscaledTotalTime => totalGameTime;        //returns unscaled total time since game started in ms
+        public float ElapsedGameTime => elapsedGameTime * timeScale;    //returns scaled time between updates in ms
+        public float TotalGameTime => totalGameTime * timeScale;    //returns scaled total time since game started in ms
+
+        public static Time Instance
+        {
+            get
+            {
+                if (instance == null)
+                    throw new NullReferenceException("Instance is null. Has GetInstance been called at startup?");
+
+                return instance;
+            }
+        }
+
+        #endregion Properties
+
+        #region Constructors
+
+        public static Time GetInstance(Game game)
+        {
+            if (instance == null)
+                instance = new Time(game);
+
+            return instance;
+        }
+
+        private Time(Game game) : base(game)
+        {
+        }
+
+        #endregion Constructors
+
+        public override void Update(GameTime gameTime)
+        {
+            elapsedGameTime = gameTime.ElapsedGameTime.Milliseconds;
+            totalGameTime = (float)gameTime.TotalGameTime.TotalMilliseconds;
+        }
+    }
+}
