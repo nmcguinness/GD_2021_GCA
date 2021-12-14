5a1bf6eb82bf846d61143f975a2a0833ec858276
Niall McGuinness <niall.mcguinness@dkit.ie>
2021-12-14 10:33:12 +0000

    Removed unnecessary if...else

--- GD3_2021_GDLibrary/GD3_2021_GDLibrary/Core/Graphics/Renderers/ForwardRenderer.cs
+++ GD3_2021_GDLibrary/GD3_2021_GDLibrary/Core/Graphics/Renderers/ForwardRenderer.cs
@@ -93,10 +93,7 @@ namespace GDLibrary.Renderers
                 if (material == null || renderer == null)
                     throw new NullReferenceException("This game object has no material and/or renderer!");
 
-                if (material.Alpha == 1)
-                    SetGraphicsStates(true);
-                else
-                    SetGraphicsStates(false);
+                SetGraphicsStates(material.Alpha == 1);
 
                 //access the shader (e.g. BasicEffect) for this rendered game object
                 shader = material.Shader;
